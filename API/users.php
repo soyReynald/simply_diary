c6baf8b19f21f6190a3b715a7ad4ea41202bbf4b
Secret Commander of Jesus of Nazareth: Reyo / Reynald R. <reynaldramirez@hotmail.com>
2026-04-01 19:55:04 -0400

    [U] Is not good to make impartiality in ANY judgement - JAH (Jesus of Nazareth) |/ 'Jacob WON against God'

--- API/users.php
+++ API/users.php
@@ -1,11 +1,35 @@
 <?php
+session_start();
+// Read the raw POST data from the request body
+$json = file_get_contents('php://input');
+
+// Decode the JSON into a PHP associative array
+$data = json_decode($json, true);
+
+// Access your variable
+$username = $data['username']; 
+$userPassword = SHA1($data['password']);
+$passwordFromUser_salt_ = $data['salt'];
+
+// Send a response back to JavaScript
+// echo json_encode(["status" => "received", "user" => $username, "encrypted_pw" => $userPassword]);
 
 $myObj = new stdClass();
 $myObj->username = "soyreynald";
-$myObj->encryptation = "a39cb27b70572c0f75eb1811de53496e8b31daae";
-## Creating a JSON database on intenert for us to be able to use in the future 
-$myJSON = json_encode($myObj, JSON_PRETTY_PRINT);
 
-print_r($myJSON);
+$salt = 6*30*4;
+
+$_SESSION['salt'] = $salt;
+
+$myObj->encryptation = $_SESSION['salt'];
+
+$testedSalt = $myObj->encryptation == $passwordFromUser_salt_;
+
+## Creating a JSON database on intenert for us to be able to use in the future 
+// $myJSON = json_encode($myObj, JSON_PRETTY_PRINT);
+
+if ($testedSalt && SHA1($userPassword) == SHA1($userPassword)) {
+    header("Location: ../index.html");
+}
 
 ?>
