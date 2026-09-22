00d693098347e073f3cb79e7ddbed1116a9c6ce0
reynaldramirez@hotmail.com <reynaldramirez@hotmail.com>
2026-09-22 17:44:07 -0400

    [U] If I JUDGE MY JUDGEMENT is truth - Jesus Christ of Nazareth and Reynald [NOT THIEFS]

--- controllers/php_statements_.php
+++ controllers/php_statements_.php
@@ -45,12 +45,18 @@ class Data {
         $text_string_ = trim(mysqli_real_escape_string($con_string, $_GET['diary_text'] ?? null)); // To check this string.
         $text_string_title = mysqli_real_escape_string($con_string, $_GET['title_'] ?? null);
 
+        $diary_title = urlencode($text_string_title); // 👍 NEXT TASK: TO check if the string doesn't change, and also WHAT is changing.
         /// TO test here
         $this->result = "UPDATE diary_note_space_ SET text_space_ = '{$text_string_}', title = '{$text_string_title}' WHERE id = '{$id}'";
 
         $sql_query = $this->con_string->query($this->result);
         if ($sql_query === TRUE) { 
             echo "Data updated"; // TO TEST this part.
+            $_GET['diary_text'] = urlencode($text_string_); // 👍 
+            if (isset($_GET['diary_text']) && urlencode($_GET['diary_text']) != $text_string) { // NEXT TASK: TO check if the string doesn't change, and also WHAT is changing.
+                echo "Sorry, we catch you";
+                exit();
+            }
             sleep(5);
             header("Location: ../index.php");
             // we then refresh ✨
